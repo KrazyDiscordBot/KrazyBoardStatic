8b96b5f37cf41cc38b1c128fb2fd6f6267f271b5
Glitch (hello-webpage) <none>
2020-11-15 15:01:27 +0000

    🍄🍁 Checkpoint ./index.html:18333624/7201 ./index.css:18333624/8355 ./command.html:18333624/79107 ./wavedark.svg:18333624/366 ./wavelight.svg:18333624/366 ./script.js:18333624/3979

---
+++ script.js
@@ -0,0 +1,91 @@
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+    selElmnt = x[i].getElementsByTagName("select")[0];
+    ll = selElmnt.length;
+    /*for each element, create a new DIV that will act as the selected item:*/
+    a = document.createElement("DIV");
+    a.setAttribute("class", "select-selected");
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+    x[i].appendChild(a);
+    /*for each element, create a new DIV that will contain the option list:*/
+    b = document.createElement("DIV");
+    b.setAttribute("class", "select-items select-hide");
+    for (j = 1; j < ll; j++) {
+        /*for each option in the original select element,
+        create a new DIV that will act as an option item:*/
+        c = document.createElement("DIV");
+        c.innerHTML = selElmnt.options[j].innerHTML;
+        c.addEventListener("click", function (e) {
+            /*when an item is clicked, update the original select box,
+            and the selected item:*/
+            var y, i, k, s, h, sl, yl;
+            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+            sl = s.length;
+            h = this.parentNode.previousSibling;
+            for (i = 0; i < sl; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("same-as-selected");
+                    yl = y.length;
+                    for (k = 0; k < yl; k++) {
+                        y[k].removeAttribute("class");
+                    }
+                    let cats = ["anime", "botinfo", "botsupport", "config", "economy", "fun", "game", "general", "image", "moderation", "servermanagement", "xpsystem", "others"]
+                    this.setAttribute("class", "same-as-selected");
+                    let pp = document.getElementsByClassName(this.innerHTML.toLowerCase()).item(0);
+                    if(!pp)pp = {innerHTML:" "};
+                    cats.filter((v)=>v!==pp.innerHTML.toLowerCase()).forEach((v)=>{
+                        let oo = document.getElementsByClassName(v).item(0);
+                        if(oo){
+                            if(oo.classList.contains("commands-visible")){
+                                oo.classList.remove("commands-visible")
+                            }
+                        }
+                    })
+                    if (pp && pp.classList) {
+                        if(pp.classList.contains("commands-visible") == false){
+                        pp.classList.toggle("commands-visible")
+                        }
+                    } 
+                    break;
+                }
+            }
+            h.click();
+        });
+        b.appendChild(c);
+    }
+    x[i].appendChild(b);
+    a.addEventListener("click", function (e) {
+        /*when the select box is clicked, close any other select boxes,
+        and open/close the current select box:*/
+        e.stopPropagation();
+        closeAllSelect(this);
+        this.nextSibling.classList.toggle("select-hide");
+        this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+    /*a function that will close all select boxes in the document,
+    except the current select box:*/
+    var x, y, i, xl, yl, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    xl = x.length;
+    yl = y.length;
+    for (i = 0; i < yl; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
+    }
+    for (i = 0; i < xl; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
+    }
+} document.addEventListener("click", closeAllSelect);
